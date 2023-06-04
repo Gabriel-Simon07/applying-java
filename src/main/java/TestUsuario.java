f445ee04a0d081141fe102a3f7d17b359956fccd
Gabriel Simon <gabrielsimon775@gmail.com>
2023-06-03 23:03:50 -0300

    Implemented unit tests

--- src/main/java/TestUsuario.java
+++ src/main/java/TestUsuario.java
@@ -2,19 +2,22 @@ import java.util.Arrays;
 
 public class TestUsuario {
     public static void main(String[] args) {
-        final var usuario1 = new Usuario("Teste1", 10, true);
-        final var usuario2 = new Usuario("Teste2", 20, false);
-        final var usuario3 = new Usuario("Teste3", 30, false);
+//        final var usuario1 = new Usuario("Teste1", 10, true);
+//        final var usuario2 = new Usuario("Teste2", 20, false);
+//        final var usuario3 = new Usuario("Teste3", 30, false);
+//
+//        final var listUsuarios = Arrays.asList(usuario1, usuario2, usuario3);
+//
+//        Mostrador mostrador = new Mostrador();
+//        listUsuarios.forEach(mostrador);
+//
+//        listUsuarios.forEach(usuario -> System.out.println(usuario.getNome()));
+//
+//        Validator<String> validatorCEP = s -> s.matches("[0-9]{5}-[0-9]{3}");
+//        System.out.println(validatorCEP.valida("88055-009"));
 
-        final var listUsuarios = Arrays.asList(usuario1, usuario2, usuario3);
-
-        Mostrador mostrador = new Mostrador();
-        listUsuarios.forEach(mostrador);
-
-        listUsuarios.forEach(usuario -> System.out.println(usuario.getNome()));
-
-        Validator<String> validatorCEP = s -> s.matches("[0-9]{5}-[0-9]{3}");
-        System.out.println(validatorCEP.valida("88055-009"));
+        Runnable run = () -> System.out.println("Opa");
+        System.out.println(run.getClass());
     }
 }
 
---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class AccountTest {
+
+    @Test
+    public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+        final var account = new Account(1L, 0.0);
+        final var expectAmount = 196.0;
+
+        account.deposit(200.0);
+
+        assertEquals(expectAmount, account.getBalance());
+    }
+
+    @Test
+    public void depositShouldNotIncreaseBalanceWhenNegativeAmount() {
+        final var account = new Account(1L, 0.0);
+
+        assertThrows(IllegalArgumentException.class, () -> account.deposit(-200.0));
+    }
+}
