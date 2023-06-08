5501e69cf32c36e507f91be1094a4e7db14532e0
Gabriel Simon <gabrielsimon775@gmail.com>
2023-06-08 15:49:39 -0300

    UsuarioFactory class implemented and TestUsuario class refactored

---
+++ src/main/java/UsuarioFactory.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class UsuarioFactory {
+    public static Usuario createUsuario(String nome, int pontos, boolean moderador) {
+        return new Usuario(nome, pontos, moderador);
+    }
+
+    public static List<Usuario> usuariosList() {
+        final var usuario1 = createUsuario("Usuario 1", 10, false);
+        final var usuario2 = createUsuario("Usuario 2", 20, true);
+        final var usuario3 = createUsuario("Usuario 3", 30, false);
+
+        return List.of(usuario1, usuario2, usuario3);
+    }
+}
