385a1d9acdeabcec78a7f39010a08eb6e299be56
Gabriel Simon <gabrielsimon775@gmail.com>
2023-06-07 23:40:09 -0300

    Financing class implemented

---
+++ src/main/java/Financing.java
@@ -0,0 +1,13 @@
+public class Financing {
+    private Double totalAmount;
+    private Double income;
+    private Integer months;
+
+    public double entry() {
+        return 0.0;
+    }
+
+    public double quota() {
+        return 0.0;
+    }
+}
