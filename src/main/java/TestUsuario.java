b7f4ab898dfb67507a77c2a77604406a48fc4db4
Gabriel Simon <gabrielsimon775@gmail.com>
2023-06-08 16:00:39 -0300

    Using andThen of interface Consumer

--- src/main/java/TestUsuario.java
+++ src/main/java/TestUsuario.java
@@ -1,11 +1,14 @@
+import java.util.function.Consumer;
+
 public class TestUsuario {
     public static void main(String[] args) {
         final var listUsuarios = UsuarioFactory.usuariosList();
 
-        Mostrador mostrador = new Mostrador();
-        listUsuarios.forEach(mostrador);
+        Consumer<Usuario> mostraMensagem = value -> System.out.println("Antes de imprimir");
 
-        listUsuarios.forEach(usuario -> System.out.println(usuario.getNome()));
+        Consumer<Usuario> imprimiNome = usuario -> System.out.println(listUsuarios.get(0).getNome());
+
+        listUsuarios.forEach(mostraMensagem.andThen(imprimiNome));
     }
 }
 
